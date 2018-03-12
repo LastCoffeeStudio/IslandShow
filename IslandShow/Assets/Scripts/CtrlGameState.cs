806bed6b5027844f39d44b4d6206d842afdc14df
carlos2380 <carlos.2380@hotmail.com>
2018-03-12 03:15:24 +0100

    [LCS-28] Win game script and Dummy object

---
+++ IslandShow/Assets/Scripts/CtrlGameState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CtrlGameState : MonoBehaviour {
+
+    public enum gameStates
+    {
+        ACTIVE,
+        PAUSE,
+        DEBUG,
+        WIN,
+        EXIT
+    }
+
+    public  gameStates gameState;
+	// Use this for initialization
+	void Start ()
+	{
+	    gameState = gameStates.ACTIVE;
+
+	}
+
+    public gameStates getGameState()
+    {
+        return gameState;
+    }
+
+    public void setGameState(gameStates newGameState)
+    {
+        switch (newGameState)
+        {
+            case gameStates.ACTIVE:
+            break;
+            case gameStates.PAUSE:
+                break;
+            case gameStates.DEBUG:
+                break;
+            case gameStates.WIN:
+                print("YOU WIIIIINNN!!!!");
+                break;
+            case gameStates.EXIT:
+                break;
+        }
+    }
+}
--- IslandShow/Assets/Scripts/CtrlMenuButtons.cs
+++ IslandShow/Assets/Scripts/CtrlMenuButtons.cs
@@ -6,21 +6,12 @@ using UnityEngine.UI;
 public class CtrlMenuButtons : MonoBehaviour
 {
     public  Main main;
-    public Button startButton;
-    public Button optionsButton;
-    public Button creditsButton;
-    public Button exitButton;
 
     // Use this for initialization
     void Start ()
     {
         main = FindObjectOfType<Main>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void clickStart()
     {
