5753851f0277bd9b09d23eb5f5ba553880b86aae
Nyghor <dmateutudela@gmail.com>
2018-03-12 14:37:28 +0100

    Added switch ending cameras when winning

--- IslandShow/Assets/Scripts/CtrlGameState.cs
+++ IslandShow/Assets/Scripts/CtrlGameState.cs
@@ -14,12 +14,22 @@ public class CtrlGameState : MonoBehaviour {
     }
 
     public  gameStates gameState;
+    public GameObject[] endCameras;
+    public float timeEndCamera;
+
+    private uint activeEndCamera;
+    private float timerEndCamera;
+    
 	// Use this for initialization
 	void Start ()
 	{
 	    gameState = gameStates.ACTIVE;
-
-	}
+        if (endCameras.Length < 0)
+        {
+            timerEndCamera = 0f;
+            activeEndCamera = (uint)Random.Range(0, endCameras.Length);
+        }
+    }
 
     public gameStates getGameState()
     {
@@ -38,6 +48,35 @@ public class CtrlGameState : MonoBehaviour {
                 break;
             case gameStates.WIN:
                 print("YOU WIIIIINNN!!!!");
+                break;
+            case gameStates.EXIT:
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        switch (gameState)
+        {
+            case gameStates.ACTIVE:
+                break;
+            case gameStates.PAUSE:
+                break;
+            case gameStates.DEBUG:
+                break;
+            case gameStates.WIN:
+                timerEndCamera -= Time.deltaTime;
+                if (timerEndCamera <= 0f)
+                {
+                    if (endCameras.Length > 0)
+                    {
+                        endCameras[activeEndCamera].SetActive(false);
+                        activeEndCamera = (uint)Random.Range(0, endCameras.Length);
+                        endCameras[activeEndCamera].SetActive(true);
+                    }
+
+                    timerEndCamera = timeEndCamera;
+                }
                 break;
             case gameStates.EXIT:
                 break;
